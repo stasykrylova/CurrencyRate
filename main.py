b95873fd96a6e323ab036a92aa40e598c9cf2da4
Stasy <55661709+stasykrylova@users.noreply.github.com>
2021-04-16 18:52:49 +0300

    Add files via upload
    New table roles, in which are roles like administrator, user and redactor. Some func for operate them and 3 new types of MenuWindow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, RiseInTransition
 from kivy.lang import Builder
 import sqlite3
 import plyer
@@ -8,14 +8,14 @@ import plyer
 class MainWindow(Screen):
     password_db = ''
 
-    def into_sys(self):
+    def authorise(self):
         if self.user_name.text != "":
             if self.user_password.text != "":
-                self.password_db = self.check(self.user_name.text)
-                if self.password_db[0][0] != '':
-
-                    if self.password_db[0][0] == self.user_password.text:
+                data_db = self.check(self.user_name.text)
+                if data_db:
+                    if data_db[0][1] == self.user_password.text:
                         print("Вы вошли в систему!")
+                        self.new_page_open(data_db[0][2])
                     else:
                         plyer.notification.notify(title='Внимание!', message="Неправильный пароль!")
                 else:
@@ -25,12 +25,30 @@ class MainWindow(Screen):
         else:
             plyer.notification.notify(title='Внимание!', message="Логин должен быть заполнен!")
 
-    def add_user(self):
+    def register(self):
+        if self.user_name.text != "":
+            if self.user_password.text != "":
+                user_pas_db = self.check(self.user_name.text)
+                if not user_pas_db:
+                    self.add_user(self.user_name.text, self.user_password.text)
+                    plyer.notification.notify(title='Успех!',
+                                              message="Пользователь зарегестрирован! Теперь войдите в систему!")
+                    self.user_name.text = ""
+                    self.user_password.text = ""
+                else:
+                    plyer.notification.notify(title='Внимание!', message="Пользователь уже зарегестрирован!")
+            else:
+                plyer.notification.notify(title='Внимание!', message="Пароль должен быть заполнен!")
+        else:
+            plyer.notification.notify(title='Внимание!', message="Логин должен быть заполнен!")
+
+    @staticmethod
+    def add_user(user_name, user_password):
         con = sqlite3.connect('user.db')
         cur = con.cursor()
         cur.execute(""" INSERT INTO id (user,password)
                 VALUES (?,?)
-        """, (self.user_name.text, self.user_password.text)
+        """, (user_name, user_password)
                     )
         con.commit()
         con.close()
@@ -39,15 +57,34 @@ class MainWindow(Screen):
     def check(user_name):
         con = sqlite3.connect('user.db')
         cur = con.cursor()
-        sql = "SELECT password FROM id WHERE user = ?"
+        sql = "SELECT * FROM id WHERE user = ?"
         cur.execute(sql, [user_name])
         result = cur.fetchall()
         con.commit()
         con.close()
         return result
 
+    def new_page_open(self, code):
+        if code == 100:
+            self.manager.transition = RiseInTransition(duration=0.5)
+            self.manager.current = 'menu_usr'
+        elif code == 200:
+            self.manager.transition = RiseInTransition(duration=0.5)
+            self.manager.current = 'menu_red'
+        elif code == 300:
+            self.manager.transition = RiseInTransition(duration=0.5)
+            self.manager.current = 'menu_adm'
 
-class Registration(Screen):
+
+class MenuWindowForAdministrator(Screen):
+    pass
+
+
+class MenuWindowForRedactor(Screen):
+    pass
+
+
+class MenuWindowForUser(Screen):
     pass
 
 
@@ -64,9 +101,26 @@ class MainApp(App):
         con = sqlite3.connect('user.db')
         cur = con.cursor()
         cur.execute(""" CREATE TABLE IF NOT EXISTS id(
-            user text,
-            password text)
+            USER text,
+            password text,
+            role int NOT NULL DEFAULT 100,
+            PRIMARY KEY (USER)
+            FOREIGN KEY (role) REFERENCES roles(ID)
+            )
             """)
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS roles(
+            ID int,
+            role text,
+            PRIMARY KEY (ID)
+            )
+            """)
+        try:
+            cur.execute(""" INSERT INTO roles (role,id)
+                    VALUES ("administrator", 300), ("user", 100), ("redactor", 200)
+            """)
+        except sqlite3.IntegrityError:
+            pass
         con.commit()
         con.close()
         return kv
